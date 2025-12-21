a75641fae9334f5dd4a445b2b0055c3640758455
Alvy <alvyahmed03@gmail.com>
2025-12-21 23:30:40 +0600

    Backend deploy

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.use(cors());
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./fb-adminsdk.json");
+// const serviceAccount = require("./fb-adminsdk.json");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+    "utf-8"
+);
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
